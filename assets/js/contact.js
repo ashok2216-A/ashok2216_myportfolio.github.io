622ba2f21e2776189dcba1194f3246601e5facb2
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2022-05-08 15:14:01 +0530

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+<form onsubmit="return false">
+</form>
+ <input type="radio" id="male" name="gender" value="male"> Male
+ <input type="radio" id="female" name="gender" value="female"> Female
+ <br><br>
+ let genderValue = male.checked ? "male" : "female"
